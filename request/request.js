fa4146196c9598f6414b90def05b02c462caf30b
Sherwood <54739986+Cwaty@users.noreply.github.com>
2021-03-13 12:02:44 +0300

    Update request.js

--- request/request.js
+++ request/request.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 
-let SunucunuzunID  = "603972&lang_code=&map=1&version=1&votes=1&plist=1&connect=1&button_name=Connect&color1=ff8415&color2=333&color3=333&color4=fff&color5=fff&color6=8a8e94&color7=8a8e94&color8=8a8e94&color9=fff&title=[TR] Wizardry Hogwarts Roleplay #YAS SINIRSIZ"
+let SunucunuzunID  = "" ///// Buraya sunucunuzun api adresini (index.php?id=')'den sonrasını giriyorsunuz!
 
 client.on('message', message => {
   if (message.content === "İstediğiniz") {
