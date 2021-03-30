55a3e0ef4a7b0c2d93c1dd0a363ccdcf5586a136
Sherwood <54739986+Cwaty@users.noreply.github.com>
2021-03-30 22:59:18 +0300

    Update gamedig.js

--- gamedig/gamedig.js
+++ gamedig/gamedig.js
@@ -10,8 +10,13 @@ Gamedig.query({
     type: 'Sherwood ', //// Buraya sunucunuzun türünü örnek : Gmod , Fivem , Minecraft
     host: 'Sherwood X Wizardry' ////// Buraya sunucunuzun hostunu
 }).then((state) => {
+    ////// buraya istediğiniz örnek bir şeyi girebilirsiniz:
+    //// örnek message.channel.send(`${state.connect} (Sunucunun ip adresini gösterir) 2. bir örnek verelim ${state.ping} (Sunucunun pingini gösterir)`)
     console.log(state);
 }).catch((error) => {
+    ///// 2. bir nokta üstte girdiğimiz state.connect , state.ping gibi değişkenler sunucu kapandığı zaman çalışmaz , ondan dolayı bilgileri buraya state.ping olarak değil de elinizle yazmanız gerekir genelde 
+    /// örnek : message.channel.send(new MessageEmbed().setDescription(`Sunucunun IP adresi : 111.111.111 , Sunucunun haritası : Harita adı.`)) gibi şeyleri elinizle girebilirsiniz
+    //// genelde botun durumunu ayarlamak için bu modül kullanılır kullanıcı sayısı [0 / maxplayer] olduğu zaman oyuncuların kafası karışabilir ondan dolayı botun durumu olarak kullanıcaksanız **Sunucu Kapalı** olarak kullanmanızı tavsiye ederim.
     console.log("Sunucu aktif değil!");
 });
 
